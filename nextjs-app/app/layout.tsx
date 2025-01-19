9ebeda71a1aeab6a1c6930cad7aa95071b568b00
Milton Mannarino <grountish@gmail.com>
2025-01-19 12:51:47 +0100

    Refactor footer layout and update project name in package-lock.json

--- nextjs-app/app/layout.tsx
+++ nextjs-app/app/layout.tsx
@@ -1,20 +1,20 @@
-import "./globals.css";
+import './globals.css';
 
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { draftMode } from "next/headers";
-import { VisualEditing, toPlainText } from "next-sanity";
-import { Toaster } from "sonner";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { draftMode } from 'next/headers';
+import { VisualEditing, toPlainText } from 'next-sanity';
+import { Toaster } from 'sonner';
 
-import DraftModeToast from "@/app/components/DraftModeToast";
-import Footer from "@/app/components/Footer";
-import Header from "@/app/components/Header";
-import * as demo from "@/sanity/lib/demo";
-import { sanityFetch, SanityLive } from "@/sanity/lib/live";
-import { settingsQuery } from "@/sanity/lib/queries";
-import { resolveOpenGraphImage } from "@/sanity/lib/utils";
-import { handleError } from "./client-utils";
+import DraftModeToast from '@/app/components/DraftModeToast';
+import Footer from '@/app/components/Footer';
+import Header from '@/app/components/Header';
+import * as demo from '@/sanity/lib/demo';
+import { sanityFetch, SanityLive } from '@/sanity/lib/live';
+import { settingsQuery } from '@/sanity/lib/queries';
+import { resolveOpenGraphImage } from '@/sanity/lib/utils';
+import { handleError } from './client-utils';
 
 /**
  * Generate metadata for the page.
@@ -52,20 +52,16 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-  display: "swap",
+  variable: '--font-inter',
+  subsets: ['latin'],
+  display: 'swap',
 });
 
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const { isEnabled: isDraftMode } = await draftMode();
 
   return (
-    <html lang="en" className={`${inter.variable} bg-white text-black`}>
+    <html lang="en" className={`${inter.variable} bg-[#712538] text-black`}>
       <body>
         <section className="min-h-screen pt-24">
           {/* The <Toaster> component is responsible for rendering toast notifications used in /app/client-utils.ts and /app/components/DraftModeToast.tsx */}
