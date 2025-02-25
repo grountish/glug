cce4d5f3f476e720011a2fb4a6849278732d7b61
yolaresofia <yolaresofia@gmail.com>
2025-02-25 11:15:23 +0100

    fix build

--- nextjs-app/app/[slug]/page.tsx
+++ nextjs-app/app/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { redirect } from "next/navigation"; // Import redirect function
 import Head from "next/head";
 
 import PageBuilderPage from "@/app/components/PageBuilder";
@@ -7,17 +6,20 @@ import { sanityFetch } from "@/sanity/lib/live";
 import { getPageQuery, pagesSlugs } from "@/sanity/lib/queries";
 import { Page as PageType } from "@/sanity.types";
 import { client } from "@/sanity/lib/client";
+import { redirect } from "next/navigation";
 
 type Props = {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 };
 
 /**
  * Generate the static params for the page.
+ * Learn more: https://nextjs.org/docs/app/api-reference/functions/generate-static-params
  */
 export async function generateStaticParams() {
   const { data } = await sanityFetch({
     query: pagesSlugs,
+    // // Use the published perspective in generateStaticParams
     perspective: "published",
     stega: false,
   });
@@ -26,11 +28,14 @@ export async function generateStaticParams() {
 
 /**
  * Generate metadata for the page.
+ * Learn more: https://nextjs.org/docs/app/api-reference/functions/generate-metadata#generatemetadata-function
  */
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
+export async function generateMetadata(props: Props): Promise<Metadata> {
+  const params = await props.params;
   const { data: page } = await sanityFetch({
     query: getPageQuery,
     params,
+    // Metadata should never contain stega
     stega: false,
   });
 
@@ -40,24 +45,22 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   } satisfies Metadata;
 }
 
-export default async function Page({ params }: Props) {
-  // Handle known error pages
+export default async function Page(props: Props) {
+  const params = await props.params;
   if (params.slug === "404") return null;
   if (params.slug === "500") return null;
 
-  // Fetch page data from Sanity
+  // use params.slug to fetch the page data
   const page = await client.fetch(
     '*[_type == "page" && slug.current == $slug][0]',
     { slug: params.slug }
   );
 
-  // **Redirect to homepage if the page does not exist**
-  if (!page?._id) {
-    redirect("/"); // Redirect to homepage
-  }
-
+  if (!page?._id) redirect("/");
   return (
-    <div className={`font-teachers bg-[${page.pageBackgroundColor.hex}]`}>
+    <div
+      className={`font-teachers bg-[${page.pageBackgroundColor.hex}]`}
+    >
       <Head>
         <title>{page.heading}</title>
       </Head>
